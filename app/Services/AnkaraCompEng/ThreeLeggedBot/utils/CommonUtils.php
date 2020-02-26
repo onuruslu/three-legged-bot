faf90a15cb32442d3967b7191fe3f0669de563f9
Onur Uslu <onrslu@gmail.com>
2020-02-26 22:27:55 +0300

    Create CommonUtils.php

---
+++ app/Services/AnkaraCompEng/ThreeLeggedBot/utils/CommonUtils.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services\AnkaraCompEng\ThreeLeggedBot\Utils;
+
+class CommonUtils{
+	protected static function array_multi_key_exists($arrNeedles, $arrHaystack)
+	{
+		$needle = array_shift($arrNeedles);
+
+		if(count($arrNeedles) == 0) {
+			return(array_key_exists($needle, $arrHaystack));
+		}
+		else {
+			if(! array_key_exists($needle, $arrHaystack)) {
+				return false;
+			} else {
+				return self::array_multi_key_exists($arrNeedles, $arrHaystack[$needle]);
+			}
+		}
+	}
+}
+
+?>
