fba6ceb3bfc152569db03b67e0ba5485843543b3
Onur Uslu <onrslu@gmail.com>
2020-03-13 20:18:03 +0300

    Update Test is added.

---
+++ tests/Feature/Update/UpdateTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature\Update;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\Feature\FeatureTestCase;
+use App\Services\AnkaraCompEng\ThreeLeggedBot\Handlers\WebhookHandler;
+use App\Services\AnkaraCompEng\ThreeLeggedBot\Objects\Update;
+use Tests\Feature\DemoFile;
+use App\User;
+
+class UpdateTest extends FeatureTestCase
+{
+	CONST DEMO_FILE_PATH	= [__DIR__, 'DemoRequest', 'start_command.json'];
+
+	public function testFromReceivingUpdateToUsingThreeleggedbotfacade() : void
+	{
+		// message parsing
+		$demoFile	= new DemoFile(...self::DEMO_FILE_PATH);
+		$demoFile->render();
+		$request	= $demoFile->getParsedTemplate();
+
+		// creating fake user
+		$fakeUser	= factory(User::class)->make();
+		$fakeUser->telegram_id			= $demoFile->properties->source_chat_id;
+		$fakeUser->telegram_username	= $demoFile->properties->source_username;
+
+		// mocking
+		$mockedThreeLeggedBotFacade = $this->mock('alias:App\Facades\ThreeLeggedBotFacade');
+
+		$mockedThreeLeggedBotFacade
+			->shouldReceive('createOrUpdateUser')
+			->andReturn($fakeUser);
+
+		$mockedWebHookHandler = $this->mock(WebhookHandler::class)->makePartial();
+
+		$mockedWebHookHandler
+			->shouldReceive('handleCommand')
+			->withArgs(
+				function($update) {
+					return is_a($update, Update::class);
+				}
+			)
+			->once();
+
+		$this->app->instance(WebhookHandler::class, $mockedWebHookHandler);
+
+		// request control
+		$response = $this->sendUpdate($request);
+
+		$response->assertStatus(200);
+
+		$response->assertSeeText('ok');
+	}
+}
