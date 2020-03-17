3833a2d555fac1b9a8a9eb7fadc9f8a19c3f8a73
Onur Uslu <onrslu@gmail.com>
2020-03-17 18:24:26 +0300

    Scan.php is updated to work with exist records on the DB.

--- app/Console/Commands/AnkaraCompEng/LessonPages/Scan.php
+++ app/Console/Commands/AnkaraCompEng/LessonPages/Scan.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands\AnkaraCompEng\LessonPages;
 
 use Illuminate\Console\Command;
 use App\Jobs\ScanSemester;
+use App\Services\AnkaraCompEng\ScanningLessonPages\Entities\Semester;
 
 class Scan extends Command
 {
@@ -38,12 +39,67 @@ class Scan extends Command
      */
     public function handle()
     {
+        if ( Semester::exists() && $this->confirm('Do you want to update the semesters on the DB?') )
+        {
+            $this->updateSemester();
+        }
+        else {
+            $this->createSemester();
+        }
+
+    }
+
+    /**
+     * Updates the semester, level, and lesson records on the DB
+     * 
+     * @return void
+     */
+    protected function updateSemester() : void
+    {
+        // Show semesters on the DB
+        $semesters              = Semester::select('id', 'title', 'link')->get();
+        $tableHeaders           = ['id', 'title', 'link'];
+
+        $this->table(
+            $tableHeaders,
+            $semesters->toArray()
+        );
+
+        // Get id of the semester which will be updated
+        $isInfiniteLoopActive   = true;
+
+        while ($isInfiniteLoopActive)
+        {
+
+            $selectedSemesterId  = $this->ask('Please enter the id number of the semester, which you want to update');
+
+            if (Semester::where('id', $selectedSemesterId)->exists())
+            {
+                $isInfiniteLoopActive   = false;
+            }
+            else {
+                $this->error('The given id is invalid!');
+            }
+        }
+
+        // Update the selected semester
+        ScanSemester::dispatchNow(
+            Semester::find($selectedSemesterId)->link
+        );
+    }
+
+    /**
+     * Creates a new semesters, levels, and lessons
+     * 
+     * @return void
+     */
+    protected function createSemester() : void
+    {
         $semesterLink = $this->ask(
             'What is the url of the current semester?'
             . "\n" . "(Eg: http://comp.eng.ankara.edu.tr/lisans-egitimi/ders-sayfalari/)"
         );
 
         ScanSemester::dispatchNow($semesterLink);
-
     }
 }
